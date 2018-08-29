29a28047fa9b753a741ee91d89adf1a1a029a7fb
Jean Altidor <altidorj1@gmail.com>
2018-08-29 12:07:03 -0400

    added new installs to the file

--- setup.sh
+++ setup.sh
@@ -7,11 +7,11 @@ echo "Installing Brew"
 echo "Brew Installed"
 
 echo "Installing core libraries"
-brew install cask heroku mongodb node postgresql rbenv
+brew install cask heroku mongodb node postgresql rbenv kubernetes-cli
 echo "Brew Libraries Installed"
 
 echo " Installing Development Programs..............."
-brew cask install flycut google-chrome slack gitkraken spectacle visual-studio-code mongodb-compass postman intellij-idea android-studio microsoft-office
+brew cask install grafana flycut google-chrome slack gitkraken spectacle visual-studio-code mongodb-compass postman intellij-idea android-studio microsoft-office
 echo "Installed Development Programs!!!!!"
 
 echo "Starting PostGres and MongoDB..."
